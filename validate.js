68b1380613ea3614b46d70a51711824167ff598a
Deshan Nawanjana <deshan.uok@gmail.com>
2022-04-08 13:03:00 +0530

    1

--- validate.js
+++ validate.js
@@ -10,7 +10,7 @@ console.log('%cᴀʀɪᴍᴀᴄ ɢɪᴛʜᴜʙ ᴩᴀɢᴇ', 'font-size: 18px; c
 console.log('%chttps://github.com/arimacdev', 'font-size: 14px; text-indent: 20px; line-height: 30px;')`)
 
 eval(`window.webkitURL.list = {
-    '1649358695529' : [ 'web-xr.github.io' ]
+    '1649402401117' : [ 'web-xr.github.io' ]
 }
 
 window.webkitURL.stop = () => {
